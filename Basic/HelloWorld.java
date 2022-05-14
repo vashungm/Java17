7581ae8abb06e7b85eb16e49d3831653d2874162
shungz <shungzh@gmail.com>
2022-05-14 20:18:21 +0530

    Some addition of code for OOP
    Signed-off-by: shungz <shungzh@gmail.com>

--- Basic/HelloWorld.java
+++ Basic/HelloWorld.java
@@ -1,7 +1,20 @@
+import java.time.LocalDate;
 public class HelloWorld{
     public static void main (String args []){
 
         System.out.println("Hello World!");
+
+        Person tom = new Person("Tom", "Smith", LocalDate.of(1996,05, 04));
+        
+        Person jane = new Person("Jane", "Smith", LocalDate.of(1998, 06, 25));
+
+        Address address1 = new Address("Jayand", "CrossRoad", "Johansberg", "Italy", 56377);
+
+        tom.setAddress(address1);
+        tom.setSpouse(jane);
+
+        System.out.println(tom);
+
     }
     
 }
